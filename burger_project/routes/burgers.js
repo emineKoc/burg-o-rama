60174f585fddd6461fadb5d91d8eebee6e0ddedb
emineKoc <eminekoc@gmail.com>
2016-02-22 20:24:54 -0500

    exercise 2

--- burger_project/routes/burgers.js
+++ burger_project/routes/burgers.js
@@ -1,16 +1,19 @@
 'use strict'
 var express = require('express');
 var burgers = express.Router();
-
+var burgersData = [ "dummy burger" ];
 
 burgers.route('/')
 .get((req,res)=>{
     // res.render('pages/burger_all',{data:burgerData })
-    res.send('this is my burger page whicl will list the all burgers');
+    res.send( burgersData );
   })
-.post((req,res)=>{
-    res.redirect('./');
-    console.log('I ll use this to create new burger entry')
+.post( (req,res)=>{
+  // insert our new burger into the collection
+  burgersData.push(req.body)
+// redirect to the new item (in a db, you'd return the new id)
+var newID = burgersData.length-1;
+res.redirect('./'+ newID)
 })
 
 burgers.route('/new')
@@ -18,18 +21,64 @@ burgers.route('/new')
     res.send('this will render my form ');
 })
 
+
 burgers.route('/:burgerID')
 .get((req,res)=>{
   var burgID = req.params.burgerID
-  res.send('this will read my params ' + burgID);
-})
+      if(!(burgID in burgersData)){
+        res.sendStatus(404);
+        return;
+      }
+     res.send( burgersData[burgID] )
+    })
 .put((req,res)=>{
-  console.log('this will edit one burger which is comming grom ?id/edit')
-})
+    var burgID = req.params.burgerID;
+    console.log("PUUUUUUUT", req.body)
+        // if we don't have a burger there, let's
+   if(!(burgID in burgersData)){
+        res.sendStatus(404);
+        return;
+     }
+
+        //replace the burger at :burgerID position
+        burgersData[burgID] = req.body;
+
+        //redirect to the new burger
+        res.redirect('./' + burgID)
+      })
+  .delete( (req,res)=>{
+      var burgID = req.params.burgerID;
+      console.log("deleteee")
+      // if we don't have a burger there, let's
+      if(!(burgID in burgersData)){
+        res.sendStatus(404);
+        return;
+      }
+
+      //replace the burger at :burgerID position
+      burgersData[burgID] = " ";
+
+      //redirect to the new burger
+      res.redirect('/burgers')
+    })
+
+
 
 burgers.route('/:burgerID/edit')
 .get( (req,res)=>{
-    res.send('this will edit my form which will redirect /burgers/:edit ');
+  var burgID = req.params.burgerID;
+  console.log("change", req.body)
+      // if we don't have a burger there, let's
+ if(!(burgID in burgersData)){
+      res.sendStatus(404);
+      return;
+   }
+
+      //replace the burger at :burgerID position
+      burgersData[burgID] = req.body;
+
+      //redirect to the new burger
+      res.redirect('./' + burgID)
 })
 
 
